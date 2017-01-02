f782f7d5b8967eb3204d672f09e0d412ed7915f7
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-02 12:54:28 -0600

    v400: cm->lineage
    Change-Id: I2fd7168e6c1b3c5ba3b273a0cdb46299c13398dc

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/lge/v400/full_v400.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := cm_v400
+PRODUCT_NAME := lineage_v400
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="v400" \
