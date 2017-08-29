64ab5e2df57fa03f3da2f12c97dc43c00bd1cc66
Adam Myczkowski <mycaxd511@gmail.com>
2017-08-29 09:52:53 +0100

    v400: inherit vendor/lineage
    Signed-off-by: Adam Myczkowski <mycaxd511@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/lge/v400/full_v400.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_NAME := lineage_v400
 
