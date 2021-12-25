0eb40029d6f3f3eb8b0906ec86c9729f1a5761be
AMDEV01 <16246587+amdev01@users.noreply.github.com>
2021-12-25 18:39:46 +0000

    v400: Move firmware to vendor
    *Thermal Config requires following hex edit to load config from vendor:
    sed -i "s|/system/etc/|/vendor/etc/|g" vendor/bin/thermal-engine

--- extract-files.sh
+++ extract-files.sh
@@ -4,4 +4,7 @@ set -e
 
 export VENDOR=lge
 export DEVICE=v400
+
+THERMAL_ENGINE="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/bin/thermal-engine
+sed -i 's|/system/etc|/vendor/etc|g' "$THERMAL_ENGINE"
 ./../../$VENDOR/v4xx-common/extract-files.sh $@
